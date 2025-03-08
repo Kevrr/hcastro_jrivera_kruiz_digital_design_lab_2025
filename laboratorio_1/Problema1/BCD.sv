ee61d5528af4369dd1bcf096408d41d691a68086
Kevin Ruiz <krr284@gmail.com>
2025-03-08 17:46:41 -0600

    fix to unit_seg

--- laboratorio_1/Problema1/BCD.sv
+++ laboratorio_1/Problema1/BCD.sv
@@ -1,12 +1,42 @@
 module BCD (input logic [3:0] x,
-				output logic [6:0] seg);
+				output logic [6:0] units_seg,
+				output logic [1:0] tens_seg);
 				
-	assign seg[6] = ((~x[3] & ~x[2]) | (x[3] & x[2] & ~x[0])) & ~x[1];
-	assign seg[5] = (((~x[3] & ~x[2]) | (x[3] & x[2])) & ~x[1] & x[0]) | (((~x[2] & ~x[0]) | (x[2] & x[0])) & ~x[3] & x[1]);
-	assign seg[4] = ~x[3] & ((~x[1] & x[0]) | (~x[2] & ~x[1]) | (x[2] & x[0]));
-	assign seg[3] = (~x[3] & ~x[2] & ~x[1] & x[0]) | (~x[3] & x[2] & ~x[1] & ~x[0]) | (x[2] & x[1] & x[0]) | (x[3] & ~x[2] & x[1] & x[0]);
-	assign seg[2] = (x[3] & x[2] & ~x[1] & ~x[0]) | (~x[3] & x[1] & ~x[0]) | (x[3] & x[2] & x[1]);
-	assign seg[1] = (~x[3] & x[2] & ~x[1] & x[0]) | (~x[3] & x[2] & ~x[1] & ~x[0]) | (x[3] & x[2] & ~x[1] & x[0]) | (x[3] & ~x[2] & x[1] & x[0]);
-	assign seg[0] = (~x[3] & ~x[2] & ~x[1] & x[0]) | (~x[3] & x[2] & ~x[1] & ~x[0]) | (x[3] & x[2] & ~x[1] & x[0]) |(x[3] & ~x[2] & x[1] & x[0]);
+	assign units_seg[6] = (~x[3] & ~x[1] |
+								 x[3] & x[1]) & 
+								 ~x[2];
+							
+	assign units_seg[5] = ~x[3] & ~x[2] & x[0] | 
+						       ~x[3] & ~x[2] & x[1] |
+						       ~x[3] & x[1] & x[0] | 
+						       x[3] & x[2] & ~x[1] |
+						       ~x[2] & x[1] & x[0];
+						 
+	assign units_seg[4] = x[0] |
+						       x[2] & (~x[3] & ~x[1] |
+						       x[3] & x[1]);
+						 
+	assign units_seg[3] = ~x[3] & ~x[2] & ~x[1] & x[0] |
+						       ~x[3] & x[2] & ~x[1] & ~x[0] | 
+						       ~x[3] & x[2] & x[1] & x[0] | 
+						       x[3] & x[2] & x[1] & ~x[0] |
+								 x[3] & ~x[2] & x[1] & x[0];
+						 
+	assign units_seg[2] = (~x[3] & ~x[2] & x[1] |
+						       x[3] & x[2] & ~x[1]) & 
+						       ~x[0];
+						 
+	assign units_seg[1] = (~x[3] & ~x[1] & x[0] |
+						       ~x[3] & x[1] & ~x[0] |
+						       x[3] & x[1] & x[0]) &
+						       x[2];
+						 
+	assign units_seg[0] = ~x[3] & ~x[2] & ~x[1] & x[0] |
+						       ~x[3] & x[2] & ~x[1] & ~x[0] |
+						       x[3] & x[2] & x[1] & ~x[0] |
+						       x[3] & ~x[2] & x[1] & x[0];
+								 
+	assign tens_seg[1] = x[3] & (x[2] | x[1]);
+	assign tens_seg[0] = tens_seg[1];
 	
 endmodule
