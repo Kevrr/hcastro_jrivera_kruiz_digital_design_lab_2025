e583a35fddcfa7bf4b42b34db93252cd9b47b1cf
Diefudu2 <107089556+Diefudu2@users.noreply.github.com>
2025-03-11 17:06:33 -0600

    Problema 2 suma 4 bits y tb

--- laboratorio_1/Problema_2/suma.sv
+++ laboratorio_1/Problema_2/suma.sv
@@ -1,6 +1,27 @@
-module suma(input logic a,b,c,d,e,f,g,h,output logic r,s,t,u,v,w,y,z, output logic [6:0] units_seg,output logic [1:0] tens_seg);
-//Primer bit
-assign y = (a|e)&((a&e)|b);
-assign z = (a^e)^b;
+module suma(
+    input logic [3:0] A, B,  // Entradas de 4 bits
+    output logic [4:0] S     // Salida de 5 bits (ahora sí soporta 30)
+);
+    logic C0, C1, C2, C3;  
+
+    // Primer bit
+    assign S[0] = A[0] ^ B[0]; 
+    assign C0   = A[0] & B[0];
+
+    // Segundo bit
+    assign S[1] = (A[1] ^ B[1]) ^ C0;
+    assign C1   = (A[1] & B[1]) | (C0 & (A[1] ^ B[1]));
+
+    // Tercer bit
+    assign S[2] = (A[2] ^ B[2]) ^ C1;
+    assign C2   = (A[2] & B[2]) | (C1 & (A[2] ^ B[2]));
+
+    // Cuarto bit
+    assign S[3] = (A[3] ^ B[3]) ^ C2;
+    assign C3   = (A[3] & B[3]) | (C2 & (A[3] ^ B[3]));
+
+    // Quinto bit (Cout)
+    assign S[4] = C3;  // Este es el bit extra para manejar hasta 30
 
 endmodule
+
