b096d230a99e123fc08b25fa19e868d86417227a
Kevin Ruiz <krr284@gmail.com>
2025-03-21 16:09:32 -0600

    changed tb messages

--- laboratorio_2/problema1/ALU_tb.sv
+++ laboratorio_2/problema1/ALU_tb.sv
@@ -8,57 +8,57 @@ module ALU_tb;
 		op = 4'b0000; //suma
 		a = 4'd1; b=4'd1;
 		#20
-		assert(result == 4'd2) else $error("failed %b + %b, expected = 2, got = %b", a, b, result);
-		assert(flags == 4'b0000) else $error("%b + %b flags mismatched, expected = 0000, got = %b", a, b, flags);
+		assert(result == 4'd2) else $error("failed %d + %d, expected = 2, got = %d", a, b, result);
+		assert(flags == 4'b0000) else $error("%d + %d flags mismatched, expected = 0000, got = %b", a, b, flags);
 		
 		a = 4'd7; b=4'd6;
 		#20
- 		assert(result == 4'd13) else $error("failed %b + %b, expected = 13, got = %b", a, b, result);
-		assert(flags == 4'b1001) else $error("%b + %b flags mismatched, expected = 1001, got = %b", a, b, flags);
+ 		assert(result == 4'd13) else $error("failed %d + %d, expected = 13, got = %d", a, b, result);
+		assert(flags == 4'b1001) else $error("%d + %d flags mismatched, expected = 1001, got = %b", a, b, flags);
 		
 		op = 4'b0001; //resta
 		a = 4'd4; b=4'd4;
 		#20
-		assert(result == 4'd0) else $error("failed %b - %b, expected = 0, got = %b", a, b, result);
-		assert(flags == 4'b0100) else $error("%b - %b flags mismatched, expected = 0100, got = %b", a, b, flags);
+		assert(result == 4'd0) else $error("failed %d - %d, expected = 0, got = %d", a, b, result);
+		assert(flags == 4'b0100) else $error("%d - %d flags mismatched, expected = 0100, got = %b", a, b, flags);
 		
 		a = 4'd8; b=4'd1;
 		#20
- 		assert(result == 4'd9) else $error("failed %b - %b, expected = 9, got = %b", a, b, result);
-		assert(flags == 4'b0001) else $error("%b - %b flags mismatched, expected = 0001, got = %b", a, b, flags);
+ 		assert(result == 4'd9) else $error("failed %d - %d, expected = 9, got = %d", a, b, result);
+		assert(flags == 4'b0001) else $error("%d - %d flags mismatched, expected = 0001, got = %b", a, b, flags);
 		
 		op = 4'b0010; //mult
 		a = 4'd2; b=4'd4;
 		#20
-		assert(result == 4'd8) else $error("failed %b * %b, expected = 8, got = %b", a, b, result);
-		assert(flags == 4'b1000) else $error("%b * %b flags mismatched, expected = 1000, got = %b", a, b, flags);
+		assert(result == 4'd8) else $error("failed %d * %d, expected = 8, got = %d", a, b, result);
+		assert(flags == 4'b1000) else $error("%d * %d flags mismatched, expected = 1000, got = %b", a, b, flags);
 		
 		a = 4'd6; b=4'd3;
 		#20
- 		assert(result == 4'd10) else $error("failed %b * %b, expected = 10, got = %b", a, b, result);
-		assert(flags == 4'b1001) else $error("%b * %b flags mismatched, expected = 1001, got = %b", a, b, flags);
+ 		assert(result == 4'd10) else $error("failed %d * %d, expected = 10, got = %d", a, b, result);
+		assert(flags == 4'b1001) else $error("%d * %d flags mismatched, expected = 1001, got = %b", a, b, flags);
 		
 		op = 4'b0011; //div
 		a = 4'd6; b=4'd2;
 		#20
-		assert(result == 4'd3) else $error("failed %b / %b, expected = 3, got = %b", a, b, result);
-		assert(flags == 4'b0000) else $error("%b / %b flags mismatched, expected = 0000, got = %b", a, b, flags);
+		assert(result == 4'd3) else $error("failed %d / %d, expected = 3, got = %d", a, b, result);
+		assert(flags == 4'b0000) else $error("%d / %d flags mismatched, expected = 0000, got = %b", a, b, flags);
 		
 		a = 4'd7; b=4'd3;
 		#20
- 		assert(result == 4'd2) else $error("failed %b / %b, expected = 2, got = %b", a, b, result);
-		assert(flags == 4'b0000) else $error("%b / %b flags mismatched, expected = 0000, got = %b", a, b, flags);
+ 		assert(result == 4'd2) else $error("failed %d / %d, expected = 2, got = %d", a, b, result);
+		assert(flags == 4'b0000) else $error("%d / %d flags mismatched, expected = 0000, got = %b", a, b, flags);
 		
 		op = 4'b0100; //mod
 		a = 4'd4; b=4'd3;
 		#20
-		assert(result == 4'd3) else $error("failed %b mod %b, expected = 3, got = %b", a, b, result);
-		assert(flags == 4'b0000) else $error("%b mod %b flags mismatched, expected = 0000, got = %b", a, b, flags);
+		assert(result == 4'd3) else $error("failed %d mod %d, expected = 3, got = %d", a, b, result);
+		assert(flags == 4'b0000) else $error("%d mod %d flags mismatched, expected = 0000, got = %b", a, b, flags);
 		
 		a = 4'd2; b=4'd2;
 		#20
- 		assert(result == 4'd0) else $error("failed %b mod %b, expected = 0, got = %b", a, b, result);
-		assert(flags == 4'b0100) else $error("%b mod %b flags mismatched, expected = 0100, got = %b", a, b, flags);
+ 		assert(result == 4'd0) else $error("failed %d mod %d, expected = 0, got = %d", a, b, result);
+		assert(flags == 4'b0100) else $error("%d mod %d flags mismatched, expected = 0100, got = %b", a, b, flags);
 		
 		op = 4'b0101; //and
 		a = 4'b0111; b=4'b0110;
@@ -95,24 +95,24 @@ module ALU_tb;
 		op = 4'b1000; //sr
 		a = 4'b1100; b=4'd2;
 		#20
-		assert(result == 4'b0011) else $error("failed %b >> %b, expected = 0011, got = %b", a, b, result);
-		assert(flags == 4'b0000) else $error("%b >> %b flags mismatched, expected = 0000, got = %b", a, b, flags);
+		assert(result == 4'b0011) else $error("failed %b >> %d, expected = 0011, got = %b", a, b, result);
+		assert(flags == 4'b0000) else $error("%b >> %d flags mismatched, expected = 0000, got = %b", a, b, flags);
 		
 		a = 4'b1010; b=4'd3;
 		#20
- 		assert(result == 4'b0001) else $error("failed %b >> %b, expected = 0001, got = %b", a, b, result);
-		assert(flags == 4'b0000) else $error("%b >> %b flags mismatched, expected = 0000, got = %b", a, b, flags);
+ 		assert(result == 4'b0001) else $error("failed %b >> %d, expected = 0001, got = %b", a, b, result);
+		assert(flags == 4'b0000) else $error("%b >> %d flags mismatched, expected = 0000, got = %b", a, b, flags);
 		
 		op = 4'b1001; //sl
 		a = 4'b1100; b=4'd2;
 		#20
-		assert(result == 4'b0000) else $error("failed %b << %b, expected = 0000, got = %b", a, b, result);
-		assert(flags == 4'b0100) else $error("%b << %b flags mismatched, expected = 0100, got = %b", a, b, flags);
+		assert(result == 4'b0000) else $error("failed %b << %d, expected = 0000, got = %b", a, b, result);
+		assert(flags == 4'b0100) else $error("%b << %d flags mismatched, expected = 0100, got = %b", a, b, flags);
 		
 		a = 4'b1010; b=4'd3;
 		#20
- 		assert(result == 4'b0000) else $error("failed %b << %b, expected = 0000, got = %b", a, b, result);
-		assert(flags == 4'b0100) else $error("%b << %b flags mismatched, expected = 0100, got = %b", a, b, flags);
+ 		assert(result == 4'b0000) else $error("failed %b << %d, expected = 0000, got = %b", a, b, result);
+		assert(flags == 4'b0100) else $error("%b << %d flags mismatched, expected = 0100, got = %b", a, b, flags);
 		
 	end
 endmodule
